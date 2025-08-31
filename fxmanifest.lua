f0fa9a3ed2ae2ba783a0843122729081e6d20346
notmachoix <155906434+notmachoix@users.noreply.github.com>
2025-08-31 18:00:12 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Aqua'
+description 'Aquas Vehicle Spawner V2'
+version '2.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+files {
+    'menu/nui.html',
+    'menu/script.js',
+    'menu/style.css'
+}
